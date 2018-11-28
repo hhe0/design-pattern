349538eea50e5778418ef7216d154b6e9b797c23
hhe <deng2296917779@163.com>
2018-11-28 16:31:14 +0800

    #  增加策略模式实例

---
+++ strategy-pattern/Programmer.php
@@ -0,0 +1,89 @@
+<?php
+class Programmer
+{
+    protected $programBehavior;
+
+    protected function __construct(programBehavior $programBehavior)
+    {
+        $this->programBehavior = $programBehavior;
+    }
+
+    public function performProgramming()
+    {
+        $this->programBehavior->programming();
+    }
+}
+
+interface ProgramBehavior
+{
+    public function programming();
+}
+
+class PhpProgramming implements ProgramBehavior
+{
+    public function programming()
+    {
+        echo 'PHP NO.1.' . "\n";
+    }
+}
+
+class JavaProgramming implements ProgramBehavior
+{
+    public function programming()
+    {
+        echo 'JAVA NO.1.' . "\n";
+    }
+}
+
+class CProgramming implements ProgramBehavior
+{
+    public function programming()
+    {
+        echo 'C NO.1.' . "\n";
+    }
+}
+
+class PhpProgrammer extends Programmer
+{
+    public function __construct(PhpProgramming $programBehavior)
+    {
+        parent::__construct($programBehavior);
+    }
+}
+
+class JavaProgrammer extends Programmer
+{
+    public function __construct(JavaProgramming $programBehavior)
+    {
+        parent::__construct($programBehavior);
+    }
+}
+
+class CProgrammer extends Programmer
+{
+    public function __construct(CProgramming $programBehavior)
+    {
+        parent::__construct($programBehavior);
+    }
+}
+
+class DataProgrammer extends Programmer
+{
+    public function __construct(CProgramming $programBehavior)
+    {
+        parent::__construct($programBehavior);
+    }
+}
+
+$phpProgrammer = new PhpProgrammer(new PhpProgramming());
+$phpProgrammer->performProgramming();
+
+$javaProgrammer = new JavaProgrammer(new JavaProgramming());
+$javaProgrammer->performProgramming();
+
+$cProgrammer = new CProgrammer(new CProgramming());
+$cProgrammer->performProgramming();
+
+$dataProgrammer = new DataProgrammer(new CProgramming());
+$dataProgrammer->performProgramming();
+
